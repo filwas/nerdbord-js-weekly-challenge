745ad1087db15b8fd40cc9c904ed6477af1952cb
Filip Was <fwas.corp@gmail.com>
2023-05-22 20:13:55 +0200

    tried fixing animation

--- challenges/week-3/script.js
+++ challenges/week-3/script.js
@@ -8,34 +8,26 @@ function pop(){
     let text = document.getElementsByClassName("button-text")[0];
 
     if (!buttonStatus) {
-        try {
-            document.body.classList.remove("body-animation-reverse");
-
-        } catch (error) {}
-
         document.body.classList.add("body-animation")
         rec.classList.add("rectangle-animation")
         text.textContent = "let's close";
         but.classList.add("button-animation");
-    
         but.addEventListener("animationend", ()=>{
             but.classList.remove("button-animation")
         });
 
-        
     } else{
-        try {
-            document.body.classList.remove("body-animation");
-            rec.classList.remove("rectangle-animation")
-        } catch (error) {}
-
         document.body.classList.add("body-animation-reverse")
         rec.classList.add("rectangle-animation-reverse")
+
         text.textContent = "pop it!"
         but.classList.add("button-animation");
 
         but.addEventListener("animationend", ()=>{
             but.classList.remove("button-animation")
+            rec.classList.remove("rectangle-animation-reverse")
+            rec.classList.remove("rectangle-animation")
+            rec.classList.add("rectangle")
         });
     }
 
